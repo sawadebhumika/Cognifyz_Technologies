a13b68b3283e39254d4b7b531ef021f7dc18fea8
unknown <sawadebhumika@gmail.com>
2024-04-27 20:17:37 +0530

    updated

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -51,10 +51,10 @@ class DataIngestion:
 
 # run data ingestion
 
-if __name__=='__main__':
-    obj=DataIngestion()
-    train_data_path,test_data_path=obj.initiate_data_ingestion()
-    data_transformation = DataTransformation()
-    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
-    # model_trainer = ModelTrainer()
-    # model_trainer.initate_model_training(train_arr,test_arr)
+# if __name__=='__main__':
+#     obj=DataIngestion()
+#     train_data_path,test_data_path=obj.initiate_data_ingestion()
+#     data_transformation = DataTransformation()
+#     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+#     model_trainer = ModelTrainer()
+#     model_trainer.initate_model_training(train_arr,test_arr)
